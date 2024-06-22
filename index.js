86d21c17d7f8b0e046b7c8b086792ea3406ff93a
ekas59425 <ekaspreetatwal@gmail.com>
2024-06-22 22:22:37 +0530

    added a comment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
-
+///hello my name is eklas
 let posts = [
     {
         id: uuidv4(),
